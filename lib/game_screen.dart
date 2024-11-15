bfd397351ba2ada4f8f3c1a5291f338acc9dfc04
iamboredasf <m.raboso.2019@alumnos.urjc.es>
2024-11-15 14:00:04 +0100

    update look

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -8,12 +8,14 @@ class GameScreen extends StatefulWidget {
   State<GameScreen> createState() => _GameScreenState();
 }
 
-class _GameScreenState extends State<GameScreen> with SingleTickerProviderStateMixin {
+class _GameScreenState extends State<GameScreen>
+    with SingleTickerProviderStateMixin {
   late GameLogic gameLogic;
   final int gridSize = 5; // Tamaño de la cuadrícula de 5x5
   final double outerMargin = 16.0; // Margen alrededor de la cuadrícula
   final double innerMargin = 4.0; // Espacio entre cada casilla
-  Map<String, double> tilePositions = {}; // Guardamos las posiciones de cada tile para animación
+  Map<String, double> tilePositions =
+      {}; // Guardamos las posiciones de cada tile para animación
   late double tileSize; // Moveremos el cálculo de `tileSize`
 
   @override
@@ -87,7 +89,8 @@ class _GameScreenState extends State<GameScreen> with SingleTickerProviderStateM
           // Si encontramos un hueco vacío, movemos las fichas hacia abajo
           for (int aboveRow = row - 1; aboveRow >= 0; aboveRow--) {
             if (gameLogic.tileGrid[aboveRow][col].value != 0) {
-              gameLogic.tileGrid[row][col].value = gameLogic.tileGrid[aboveRow][col].value;
+              gameLogic.tileGrid[row][col].value =
+                  gameLogic.tileGrid[aboveRow][col].value;
               gameLogic.tileGrid[aboveRow][col].value = 0;
               break;
             }
@@ -102,7 +105,10 @@ class _GameScreenState extends State<GameScreen> with SingleTickerProviderStateM
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    double gridSizePx = screenWidth - 2 * outerMargin;
+    double gridSizePx = MediaQuery
+        .of(context)
+        .size
+        .width - 16.0 * 2;
     double containerHeight = gridSizePx;
 
     List<Widget> stackItems = [];
@@ -132,76 +138,100 @@ class _GameScreenState extends State<GameScreen> with SingleTickerProviderStateM
       for (int j = 0; j < gridSize; j++) {
         Tile tile = gameLogic.tileGrid[i][j];
         String key = "$i-$j"; // Clave única para cada tile en la cuadrícula
-        double finalTop = tilePositions[key] ?? -tileSize; // Posición inicial (fuera de la cuadrícula)
+        double finalTop = tilePositions[key] ??
+            -tileSize; // Posición inicial (fuera de la cuadrícula)
 
         stackItems.add(
           AnimatedPositioned(
-            duration: Duration(milliseconds: 300), // Duración de la animación
-            curve: Curves.easeOut, // Suavizado de la animación
-            left: j * (tileSize + innerMargin),
-            top: i * (tileSize + innerMargin), // Posición actual de la ficha
-            width: tileSize,
-            height: tileSize,
-            child: GestureDetector(
-              onHorizontalDragUpdate: (details) {
-                if (details.delta.dx > 0) {
-                  onSwipe(tile, 0, 1); // Swipe a la derecha
-                } else if (details.delta.dx < 0) {
-                  onSwipe(tile, 0, -1); // Swipe a la izquierda
-                }
-              },
-              onVerticalDragUpdate: (details) {
-                if (details.delta.dy > 0) {
-                  onSwipe(tile, 1, 0); // Swipe hacia abajo
-                } else if (details.delta.dy < 0) {
-                  onSwipe(tile, -1, 0); // Swipe hacia arriba
-                }
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                  color: numTileColor[tile.value] ?? Colors.grey,
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Center(
-                  child: Text(
-                    tile.value == 0 ? "" : tile.value.toString(),
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+              duration: Duration(milliseconds: 300),
+              // Duración de la animación
+              curve: Curves.easeOut,
+              // Suavizado de la animación
+              left: j * (tileSize + innerMargin),
+              top: i * (tileSize + innerMargin),
+              // Posición actual de la ficha
+              width: tileSize,
+              height: tileSize,
+              child: GestureDetector(
+                onHorizontalDragUpdate: (details) {
+                  if (details.delta.dx > 0) {
+                    onSwipe(tile, 0, 1); // Swipe a la derecha
+                  } else if (details.delta.dx < 0) {
+                    onSwipe(tile, 0, -1); // Swipe a la izquierda
+                  }
+                },
+                onVerticalDragUpdate: (details) {
+                  if (details.delta.dy > 0) {
+                    onSwipe(tile, 1, 0); // Swipe hacia abajo
+                  } else if (details.delta.dy < 0) {
+                    onSwipe(tile, -1, 0); // Swipe hacia arriba
+                  }
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: numTileColor[tile.value] ?? Colors.grey,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Center(
+                    child: Text(
+                      tile.value == 0 ? "" : tile.value.toString(),
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
                   ),
                 ),
-              ),
-            ),
-          ),
+              )),
         );
       }
     }
 
     // Mostrar el próximo número en la parte superior
-    Widget nextNumberDisplay = Container(
-      width: gridSizePx,
-      height: tileSize,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      child: Center(
-        child: Text(
-          gameLogic.nextNumber?.toString() ?? '',
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),
+    Widget nextNumberDisplay = Stack(
+      children: [
+        Center(
+          child: Container(
+            width: gridSizePx,
+            height: tileSize,
+            decoration: BoxDecoration(
+              color: palePink,
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+          ),
         ),
-      ),
+        Center(
+          child: Container(
+            width: tileSize,
+            height: tileSize,
+            decoration: BoxDecoration(
+              color: numTileColor[gameLogic.nextNumber],
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Center(
+              child: Text(
+                gameLogic.nextNumber?.toString() ?? '',
+                style: TextStyle(
+                    fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
 
     return Scaffold(
       appBar: AppBar(title: Text('Partida en curso')),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           SizedBox(height: 10.0),
           nextNumberDisplay, // Muestra el próximo número
           SizedBox(height: 10.0),
           Container(
-            width: gridSizePx + 2 * outerMargin,
-            height: containerHeight + 2 * outerMargin,
-            padding: EdgeInsets.all(outerMargin),
+            width: gridSizePx,
+            height: gridSizePx,
+            padding: EdgeInsets.all(4.0),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(8.0),
               color: softGreyPink,
@@ -218,7 +248,8 @@ class _GameScreenState extends State<GameScreen> with SingleTickerProviderStateM
                   height: tileSize / 2,
                   color: Colors.transparent,
                   child: Center(
-                    child: Text("↓", style: TextStyle(fontSize: 24, color: Colors.black)),
+                    child: Text("↓",
+                        style: TextStyle(fontSize: 24, color: Colors.black)),
                   ),
                 ),
               );
