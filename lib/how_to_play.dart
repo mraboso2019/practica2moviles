5d74497ae66eedbc670b4db35c78bf5f4eb12e13
mraboso2019 <m.raboso.2019@alumnos.urjc.es>
2024-11-17 20:22:14 +0100

    Update how_to_play.dart

--- lib/how_to_play.dart
+++ lib/how_to_play.dart
@@ -9,7 +9,57 @@ class HowToPlay extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
         children: [
-          Text('this is how you play'),
+          Padding(
+            padding: EdgeInsets.all(20.0), // Ajusta el valor para el espacio alrededor de los textos
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  
+                  child: Text(
+                    'HOW TO PLAY',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10), // Espacio entre los textos
+
+                Text(
+                  'Choose the column where you want the number on the screen to fall by tapping on the arrows below the grid',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+
+                ),
+                SizedBox(height: 10), // Espacio entre los textos
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.keyboard_double_arrow_down_rounded),
+                    Icon(Icons.keyboard_double_arrow_down_rounded),
+                    Icon(Icons.keyboard_double_arrow_down_rounded),
+                  ],
+                ),
+                SizedBox(height: 10), // Espacio entre los textos
+                Text(
+                  'Swipe to combine equal tiles',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+
+                ),
+              ],
+            ),
+          ),
+
           SizedBox(height: 20),
           ElevatedButton(
             onPressed: () {
