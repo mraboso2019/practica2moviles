d12d4e7a8552fe0275edbbaecfcfdfb453367e67
iamboredasf <m.raboso.2019@alumnos.urjc.es>
2024-11-19 22:50:56 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,33 +9,7 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final AssetsAudioPlayer _assetsAudioPlayer = AssetsAudioPlayer();
-
-  @override
-  void initState() {
-    super.initState();
-    // Reproducir la música de fondo en bucle al iniciar la app
-    _assetsAudioPlayer.open(
-      Audio("assets/music/background_music.mp3"), // Ruta al archivo de audio
-       // Hace que la música se repita
-      autoStart: true, // Comienza a reproducir automáticamente
-    );
-    _assetsAudioPlayer.setLoopMode(LoopMode.single);
-  }
-
-  @override
-  void dispose() {
-    // Detenemos la música al cerrar la app
-    _assetsAudioPlayer.stop();
-    super.dispose();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<AppTheme>(
