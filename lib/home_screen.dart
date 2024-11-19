66c30aac60b0ec4cc51506635c4cea58501c4cdf
iamboredasf <m.raboso.2019@alumnos.urjc.es>
2024-11-19 15:02:02 +0100

    update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -17,6 +17,7 @@ class HomeScreen extends StatelessWidget {
           children: [
             ElevatedButton(
               onPressed: () {
+
                 Navigator.of(context).push(
                   PageRouteBuilder(
                     pageBuilder: (context, animation, secondaryAnimation) =>
